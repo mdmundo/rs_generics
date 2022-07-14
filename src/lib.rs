4b430635563db3a2b748220767e0d6b8e9147c25
Edmundo Paulino <manyymoore@gmail.com>
2022-07-14 17:32:57 +0000

    feat: 🚧 Learning about generics

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,13 @@ fn once<T: Add>(a: T, b: T) -> impl Iterator<Item = <T as Add>::Output> {
     iter::once(a + b)
 }
 
+fn mix(a: impl ToString, b: impl ToString) -> String {
+    a.to_string() + b.to_string().as_str()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{largest, once};
+    use crate::{largest, mix, once};
 
     #[test]
     fn char_largest() {
@@ -31,5 +35,15 @@ mod tests {
     fn i128_once() {
         let res = once(5, 8).next().unwrap();
         assert_eq!(res, 13);
+    }
+    #[test]
+    fn u8_mix() {
+        let res = mix(5u8, "8");
+        assert_eq!(res.as_str(), "58");
+    }
+    #[test]
+    fn u32_mix() {
+        let res = mix(59u32, ": é o número da sorte");
+        assert_eq!(res.as_str(), "59: é o número da sorte");
     }
 }
